87e1e9ba6262bb72e9ffafda072af55376d4239e
delia gt <delissqt@gmail.com>
2023-01-09 17:56:50 -0600

    detail Post

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,73 @@
 from django.shortcuts import render
 
+from datetime import date
+
+posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "People of Brooklyn - Nature.png",
+        "author": "D me",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": ".............some text some text some text ", 
+        "content": """
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        """
+    },
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "People of Brooklyn - Nature.png",
+        "author": "Dd me",
+        "date": date(2020, 5, 19),
+        "title": "Mountain Hiking",
+        "excerpt": ".............some text some text some text ", 
+        "content": """
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        """
+    },
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "People of Brooklyn - Nature.png",
+        "author": "Ddd me",
+        "date": date(2012, 2, 22),
+        "title": "Mountain Hiking",
+        "excerpt": ".............some text some text some text ", 
+        "content": """
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        """
+    },
+]
+
 # Create your views here.
 
 def starting_page(request):
@@ -10,5 +78,5 @@ def posts(request):
     return render(request, "blog/all-posts.html")
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request, "blog/post-detail.html")

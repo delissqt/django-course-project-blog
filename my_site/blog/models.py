1cadc7909e4e43c390a7256ecf822fd3ee4e310a
delia gt <delissqt@gmail.com>
2023-01-23 12:28:10 -0600

    Adding a Post Model
    Review Model, Author Model And one-to-many Relatio between Author and Post Model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
+
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField(max_length=100)
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=100)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300) # TextField
+    image_name= models.CharField(max_length=100) # ImageField , ImageField.height_field, ImageField.width_field
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True, db_index=True)
+    content = models.TextField(validators=[MinValueValidator(50), MaxValueValidator(500)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+    
+    tag = models.ManyToManyField(Tag, null=False)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)

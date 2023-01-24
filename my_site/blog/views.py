0dcd7d4bd7625d74f297b3402eda9d308c1ea8d2
delia gt <delissqt@gmail.com>
2023-01-24 17:11:48 -0600

    Fetching Posts for Starting Page
    Fetching Data from Data Base

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -2,71 +2,9 @@ from django.shortcuts import render
 
 from datetime import date
 
-all_posts = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "Beep Beep - Mountains.png",
-        "author": "D me",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": ".. only text .. only text .. only text .. only text .. only text...", 
-        "content": """
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+from .models import Post
 
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        """
-    },
-    {
-        "slug": "swimming-in-the-sea",
-        "image": "Fitz - Morning Routine.png",
-        "author": "Dd me",
-        "date": date(2020, 5, 19),
-        "title": "Sea Swimming",
-        "excerpt": ".............some text some text some text ", 
-        "content": """
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        """
-    },
-    {
-        "slug": "walking-in-the-forest",
-        "image": "People of Brooklyn - Nature.png",
-        "author": "Ddd me",
-        "date": date(2012, 2, 22),
-        "title": "Forest walking",
-        "excerpt": ",,,,,,some text some,,,,,,,,,,  ", 
-        "content": """
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-        """
-    },
-]
+all_posts = []
 
 #temparally function for dummmy data
 def get_date(post):
@@ -76,8 +14,7 @@ def get_date(post):
 # Create your views here.
 
 def starting_page(request):
-    sorted_posts = sorted(all_posts, key=get_date)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", { "posts": latest_posts })
 
 

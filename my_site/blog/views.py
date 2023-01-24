8f047cfd36ff44dc2b8593b8422f177641141a6d
delia gt <delissqt@gmail.com>
2023-01-24 17:33:28 -0600

    Ysing Author And Tags Data

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from datetime import date
 
@@ -19,9 +19,11 @@ def starting_page(request):
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", { "all_posts": all_posts })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    #identified_post = Post.objects.get(slug=slug)
+    identified_post =get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {"post": identified_post})

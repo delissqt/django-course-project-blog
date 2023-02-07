037976904dc3536432b9256136e59e9cf04bb687
delia gt <delissqt@gmail.com>
2023-02-07 17:13:54 -0600

    Converting Views to Class Based Working

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -9,34 +9,43 @@ from .models import Post
 
 # Create your views here.
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html", { "posts": latest_posts })
+#def starting_page(request):
+#    latest_posts = Post.objects.all().order_by("-date")[:3]
+#    return render(request, "blog/index.html", { "posts": latest_posts })
 
-class StartingPage(ListView):
+class StartingPageView(ListView):
     template_name = "blog/index.html"
-    model = Post.objects.all().order_by("-date")[:3]
+    model = Post
+    ordering = ["-date"] # define how display data// I could add as an extra entry year, is posible added multiple ordering fields
     context_object_name = "posts"
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html", { "all_posts": all_posts })
 
-class Posts(ListView):
+
+#def posts(request):
+#    all_posts = Post.objects.all().order_by("-date")
+#    return render(request, "blog/all-posts.html", { "all_posts": all_posts })
+
+class PostsView(ListView):
     template_name = "blog/all-posts.html"
-    model = Post.objects.all().order_by("-date")
-    context_name = all_posts
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "all_posts"
 
 
-def post_detail(request, slug):
-    #identified_post = Post.objects.get(slug=slug)
-    identified_post =get_object_or_404(Post, slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tag.all() # get all tags related to this post
-        })
-        # we are querying for all the related tags
+
+#def post_detail(request, slug):
+#    #identified_post = Post.objects.get(slug=slug)
+#    identified_post =get_object_or_404(Post, slug=slug)
+#    return render(request, "blog/post-detail.html", {
+#        "post": identified_post,
+#        "post_tags": identified_post.tag.all() # get all tags related to this post
+#        })
+#        # we are querying for all the related tags
 
 
 class PostDetailView(DetailView):
@@ -45,9 +54,5 @@ class PostDetailView(DetailView):
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        loaded_view = self.object
-        request = self.request
-        post_slug = Post.objects.get(slug= kwargs.slug)
-        context["post"] = post_slug
-        context["post_tags"] = post_slug.tag.all() 
- 
+        context["post_tags"] = self.object.tag.all()
+        return context
